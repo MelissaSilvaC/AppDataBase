311d00571949accdd824f4a5a4c4e34ce9624c59
Robson Conceicâo <melissa.conceicao01@etec.sp.gov.br>
2022-09-12 20:59:39 -0300

    atualizacao

--- app/src/main/java/com/example/appsqlite/MainActivity.kt
+++ app/src/main/java/com/example/appsqlite/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.appsqlite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.example.appsqlite.db.DBHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +16,22 @@ class MainActivity : AppCompatActivity() {
         val endereco: EditText = findViewById(R.id.edtendereco)
         val bairro: EditText = findViewById(R.id.edtbairro)
         val cep: EditText = findViewById(R.id.edtcep)
+
         val btn: Button = findViewById(R.id.btnCadastrar)
 
-        btn.setOnClickListener{
+        nome.setText(intent.getStringExtra("name"))
+        endereco.setText(intent.getStringExtra("endereco"))
+        bairro.setText(intent.getStringExtra("bairro"))
+        cep.setText(intent.getStringExtra("cep"))
 
+        val intent: Intent
+        val db = DBHelper(this, null)
+
+        btn.setOnClickListener{
+            db.addPessoa(nome.text.toString(), endereco.text.toString(), bairro.text.toString(), cep.text.toString())
+            finish()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
